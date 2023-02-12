0053c54d077517fd3defcc006c8fad59e5f0f408
UriiB <1@mail.ru>
2023-02-12 22:18:56 +0300

    Вывод нового массива

--- Program.cs
+++ Program.cs
@@ -15,15 +15,20 @@ int x = Convert.ToInt32(Console.ReadLine());
 
 if (x>0)
 {
+Console.WriteLine("Введите элементы массива");    
 string[] massive = GetRandomArray(x);
 Console.WriteLine(string.Join(", ", massive));
 
 string[] array1 = new string[x];
-
+int c = 0;
 for(int i = 0; i < massive.Length; i++) 
 {
 string str = massive[i]; 
-if ((str.Length == 3) | (str.Length < 3)) array1[i] = str;
+if ((str.Length == 3) | (str.Length < 3)) { array1[c] = str; c ++;}
 } 
+ 
+Console.WriteLine("Элементы массива меньше либо равны 3");
+Console.WriteLine(string.Join(", ", array1));
 
+}
 else Console.WriteLine("Введите правильный размер массива");
