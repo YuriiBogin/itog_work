01ca77f48f5945c565c1364be58620240ea5907c
UriiB <1@mail.ru>
2023-02-12 21:45:17 +0300

    Условия отбора элементов массива

--- Program.cs
+++ Program.cs
@@ -17,5 +17,13 @@ if (x>0)
 {
 string[] massive = GetRandomArray(x);
 Console.WriteLine(string.Join(", ", massive));
-}
+
+string[] array1 = new string[x];
+
+for(int i = 0; i < massive.Length; i++) 
+{
+string str = massive[i]; 
+if ((str.Length == 3) | (str.Length < 3)) array1[i] = str;
+} 
+
 else Console.WriteLine("Введите правильный размер массива");
