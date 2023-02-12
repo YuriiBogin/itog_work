43a5d1dc4d415bf53da850865cc2bf5ad2a236bf
UriiB <1@mail.ru>
2023-02-12 21:15:48 +0300

    Создание массива строкового типа

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿string[] GetRandomArray(int size)
+{
+    string[] array = new string[size];
+
+    for (int i =0; i< array.Length; i++)
+    {
+        array[i] = Console.ReadLine();
+    }
+
+    return array;
+}
+
+Console.WriteLine("Введите размер массива");
+int x = Convert.ToInt32(Console.ReadLine());
+
+if (x>0)
+{
+string[] massive = GetRandomArray(x);
+Console.WriteLine(string.Join(", ", massive));
+}
+else Console.WriteLine("Введите правильный размер массива");
